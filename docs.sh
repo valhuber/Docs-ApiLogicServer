8c5bb92bedcf8463ecbe9ccc4da28aa0ada28bd0
Val Huber <valjhuber@gmail.com>
2022-07-04 18:51:53 -0700

    relative sh

--- docs.sh
+++ docs.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
-# sh ~/dev/Docs-ApiLogicServer/docs.sh
+# e.g.:  sh ~/dev/Docs-ApiLogicServer/docs.sh
 
-source ~/dev/Docs-ApiLogicServer/venv/bin/activate
-cd ~/dev/ApiLogicServer
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pushd $SCRIPT_DIR
+source venv/bin/activate
+cd ../ApiLogicServer
 mkdocs serve
+popd
